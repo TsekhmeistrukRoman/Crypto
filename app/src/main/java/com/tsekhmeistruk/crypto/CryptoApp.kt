8b4feb03859cedc14fdecca03cd28eef0b5a007d
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-01-05 16:56:14 +0200

    Initial commit

---
+++ app/src/main/java/com/tsekhmeistruk/crypto/CryptoApp.kt
@@ -0,0 +1,14 @@
+package com.tsekhmeistruk.crypto
+
+import android.app.Application
+
+/**
+ * Created by Roman Tsekhmeistruk on 05.01.2018.
+ */
+
+class CryptoApp : Application() {
+
+    init {
+
+    }
+}
---
+++ app/src/main/java/com/tsekhmeistruk/crypto/ui/activities/LaunchActivity.kt
@@ -0,0 +1,13 @@
+package com.tsekhmeistruk.crypto.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tsekhmeistruk.crypto.R
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
