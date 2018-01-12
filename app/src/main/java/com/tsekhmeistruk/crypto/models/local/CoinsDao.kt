109ec69c2263a687ad5f3288db649a7c8e77ac02
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2018-01-12 14:10:10 +0200

    Added CoinsDao class

---
+++ app/src/main/java/com/tsekhmeistruk/crypto/models/local/CoinsDao.kt
@@ -0,0 +1,29 @@
+package com.tsekhmeistruk.crypto.models.local
+
+import android.arch.persistence.room.*
+import android.arch.persistence.room.OnConflictStrategy.REPLACE
+import com.tsekhmeistruk.crypto.models.entities.CoinDto
+import io.reactivex.Flowable
+
+/**
+ * Created by Roman Tsekhmeistruk on 12.01.2018.
+ */
+
+@Dao
+interface CoinsDao {
+
+    @Query("select * from coins")
+    fun getAllCoins(): Flowable<List<CoinDto>>
+
+    @Query("select * from coins where id = :p0")
+    fun findCoinById(id: String): Flowable<CoinDto>
+
+    @Insert(onConflict = REPLACE)
+    fun insertCoin(coinDto: CoinDto)
+
+    @Update(onConflict = REPLACE)
+    fun updateCoin(coinDto: CoinDto)
+
+    @Delete
+    fun deleteCoin(coinDto: CoinDto)
+}
